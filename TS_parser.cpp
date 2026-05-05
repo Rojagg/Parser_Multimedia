fa1f0306ab88960f681b0c678afd7962f9860c73
Rojag <szy.pol4@gmail.com>
2026-05-05 14:32:51 +0200

    Dodanie dodatkowych funkcji z zajec - 21.04

--- TS_parser.cpp
+++ TS_parser.cpp
@@ -15,17 +15,22 @@ FILE* file = std::fopen("example_new.ts", "rb"); //read in binary mode -rb
   }
 
   xTS_PacketHeader    TS_PacketHeader;
-
+  xTS_AdaptationField TS_AdaptationField;
   int32_t TS_PacketId = 0;
   uint8_t TS_PacketBuffer[188]; // buffer 188B
   while(std::fread(TS_PacketBuffer, 1, 188, file) ==188)
   {
     // TODO - read from file
     TS_PacketHeader.Reset();
+    TS_AdaptationField.Reset();
     TS_PacketHeader.Parse(TS_PacketBuffer);
 
     printf("%010d ", TS_PacketId);
     TS_PacketHeader.Print();
+    printf(" ");
+    if(TS_AdaptationField.Parse(TS_PacketBuffer, TS_PacketHeader.getAFC() > 0)){
+    TS_AdaptationField.Print();
+    }
     printf("\n");
 
     TS_PacketId++;
--- tsTransportStream.cpp
+++ tsTransportStream.cpp
@@ -58,3 +58,59 @@ void xTS_PacketHeader::Print() const
 }
 
 //=============================================================================================================================================================================
+
+/// @brief Reset - reset all TS packet header fields
+void xTS_AdaptationField::Reset()
+{
+    m_AFL = 0;
+    m_DC = 0;
+    m_RA = 0;
+    m_SP = 0;
+    m_PR = 0;
+    m_OR = 0;
+    m_SF = 0;
+    m_EX = 0;
+}
+/**
+@brief Parse adaptation field
+@param PacketBuffer is pointer to buffer containing TS packet
+@param AdaptationFieldControl is value of Adaptation Field Control field of
+corresponding TS packet header
+@return Number of parsed bytes (length of AF or -1 on failure)
+*/
+int32_t xTS_AdaptationField::Parse(const uint8_t* PacketBuffer, uint8_t AdaptationFieldControl)
+{
+  if (AdaptationFieldControl != 2 && AdaptationFieldControl !=3) return -1;
+
+  const uint8_t* AF = PacketBuffer + xTS::TS_HeaderLength;
+
+  m_AFL = AF[0];
+
+  if(m_AFL == 0) return 1;
+  
+  uint8_t flags = AF[1];
+
+  m_DC = (flags >> 7) & 0x01;
+  m_RA = (flags >> 6) & 0x01;
+  m_SP = (flags >> 5) & 0x01;
+  m_PR = (flags >> 4) & 0x01;
+  m_OR = (flags >> 3) & 0x01;
+  m_SF = (flags >> 2) & 0x01;
+  m_TP = (flags >> 1) & 0x01;
+  m_EX = (flags >> 0) & 0x01;
+
+  return 1 + m_AFL;
+}
+/// @brief Print all TS packet header fields
+void xTS_AdaptationField::Print() const
+{
+   printf("AF: ");
+  printf("DC=%d",m_DC);
+  printf(" RA=%d",m_RA);
+  printf(" SP=%d",m_SP);
+  printf(" PR=%d",m_PR);
+  printf(" OR=%d",m_OR);
+  printf(" SF=%d",m_SF);
+  printf(" TP=%d",m_TP);
+  printf(" EX=%d",m_EX);
+}
